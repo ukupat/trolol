acd358fd47152c99382f3094d6943c3787fdf2b6
ayhankuru <ayhankuru@yandex.com.tr>
2015-11-15 16:59:16 +0200

    need steve balmer

--- src/os-specific/trolol-linux.js
+++ src/os-specific/trolol-linux.js
@@ -4,5 +4,8 @@ module.exports = {
 
     friday: function (wait) {
         exec('bash ' + __dirname + '/../scripts/linux/friday.sh ' + wait);
+    },
+    steve: function (wait) {
+        exec('bash ' + __dirname + '/../scripts/linux/steve.sh ' + wait);
     }
 };
---
+++ src/scripts/linux/steve.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+(sleep $1; xdg-open https://www.youtube.com/watch?v=d_AP3SGMxxM) &
+
+echo "Trigger has been set for $1 seconds, trololol.."
