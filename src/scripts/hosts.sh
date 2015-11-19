2a5064b6f9548ad0014642ddc23383408a420b47
Henri <henri.ots@hotmail.com>
2015-11-19 17:43:58 +0200

    Command line argument check
    Checks if 2 command line arguments are supplied

--- src/scripts/hosts.sh
+++ src/scripts/hosts.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-from=$1
-to=$2
+from=$2
+to=$1
 
-IP=$(nslookup $2 | tail -2 | awk -F ":" '{print $2}' | xargs)
-
-echo "$IP $1" >> /etc/hosts
+if [ $# -eq 2 ]  
+	then	
+		IP=$(nslookup $2 | tail -2 | awk -F ":" '{print $2}' | xargs)
+		echo "$IP $1" >> /etc/hosts
+		exit 0
+		
+	else
+		echo "Atleast 2 command line arguments have to be supplied."
+		echo "e.g. $0 delfi.ee facebook.com"
+		exit 1
+fi
